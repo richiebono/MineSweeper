96da5eda249873c93815bedadad3da4b5b186742
Richard Bono de Oliveira <richoliveira@deloitte.com>
2021-05-04 20:30:12 -0300

    Add MineSweeper.Authentication.Api

---
+++ MineSweeper.Authentication.Api/src/main/java/authentication/Application.java
@@ -0,0 +1,17 @@
+package com.authentication;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Richard Bono
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
